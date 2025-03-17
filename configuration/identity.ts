4a8e9947cb411a8e7adc94d8d9c387cf869f52e1
cbrennan23 <connormobile12@gmail.com>
2025-03-16 21:20:17 -0400

    edited name/role

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Connor Brennan`;
 export const OWNER_DESCRIPTION: string = `Student at UNC-Chapel Hill`;
 
-export const AI_NAME: string = `New Grad Financial Advisor`;
+export const AI_NAME: string = `The Recent Graduate Financial Advisor`;
 export const AI_TONE: string = `Upbeat`;
-export const AI_ROLE: string = `Help recent graduates with finance information and advice.`;
+export const AI_ROLE: string = `Help recent graduates with financial information and advice.`;
