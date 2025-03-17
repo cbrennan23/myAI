be8f6f7d9d532b4bfd661f7fa12b2224a3f33864
cbrennan23 <connormobile12@gmail.com>
2025-03-16 21:27:46 -0400

    changed tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Connor Brennan`;
 export const OWNER_DESCRIPTION: string = `Student at UNC-Chapel Hill`;
 
 export const AI_NAME: string = `The Recent Graduate Financial Advisor`;
-export const AI_TONE: string = `Upbeat`;
+export const AI_TONE: string = `Helpful`;
 export const AI_ROLE: string = `Help recent graduates with financial information and advice.`;
